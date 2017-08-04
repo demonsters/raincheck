6bbb9c1e1f45f2eb3205d1b9c4096613189dfdbe
stijn <stijn@demonsters.nl>
2017-08-04 17:45:27 +0200

    Fix copy flow script

--- scripts/copy.js
+++ scripts/copy.js
@@ -1,9 +1,14 @@
-const fs = require('fs')
 
-function copy(pathName, outName) {
-  fs.createReadStream(pathName)
-      .pipe(fs.createWriteStream(outName))
-}
+const find = require('find');
+const path = require('path');
+const fs = require('fs-extra')
 
-// Dynamic
-copy('./src/index.js.flow', './lib/index.js.flow')
+const src = path.resolve(__dirname, '../src/')
+const lib = path.resolve(__dirname, '../lib/')
+
+// Copy flow files
+find.file(/\.js.flow$/, src, function(files) {
+  files.forEach(file => {
+    fs.copy(file, path.resolve(lib, path.relative(src, file)))
+  })
+})
