1f9dc9801c47d3d90bc7c28160f509da4cc04a41
Stijn van der Laan <stijn@demonsters.com>
2017-10-14 10:32:00 +0200

    Add examples & export the functions in index

--- src/index.js
+++ src/index.js
@@ -2,11 +2,61 @@
 // TODO: More unit tests?
 // TODO: flow
 
+// act-on
+// acton
+// call-when
+// call-on
+// call-on-me :)
+// act-when
+// redux-do-when
+// act-on-state
+
+// beetje ver gezocht misschien:
+// seasonal
+
+
+// obtainer
+// redux-act-on
+// conditional-side-effects
+
+
 import doWhen from './doWhen'
+import doWhenTrue from './doWhenTrue'
 import doWhenChanged from './doWhenChanged'
-import doForAll from './doForAll'
-import doForAllKeys from './doForAllKeys'
 import createMiddleware from './createMiddleware'
+
+// TODO: Clearer names:
+import doForAll from './doForAll'
+
+
+// doWhen((state, call) => {
+//   Object.keys(state).map((object, key) => call(func, [object], item))
+// })(["key1"])
+
+// doForAll(object => func(object))(["key1": {object: "adfa"}])
+
+
+// doWhen((state, call) => {
+//   state.map(item => call(func, [item], item))
+// })(["key1"])
+
+// doForAllKeys(key => func(key))(["key1"])
+
+
+
+import doForAllKeys from './doForAllKeys'
+
+// doForList
+//
+
+export {
+  doWhen,
+  doWhenTrue,
+  doWhenChanged,
+  doForAll,
+  doForAllKeys,
+  createMiddleware
+}
 
 // doForAllObjects ??
 // doFor ??
@@ -16,3 +66,10 @@ import createMiddleware from './createMiddleware'
 // doWhen(isLoggedIn, connect).then()
 
 // doWhen(isLogginIn, connect).observe(observable => observable.)
+
+/*
+doWhenTrue()
+  .map(s => s.value)
+  .then(connect)
+  .check({value: true})
+*/
