b5d45d720c8faafab4471559ceadbb0d09d732f5
Stijn van der Laan <stijn@demonsters.com>
2017-10-14 15:47:30 +0200

    Removed some comments & add default export to index.js

--- src/index.js
+++ src/index.js
@@ -1,66 +1,10 @@
 
-// TODO: More unit tests?
-// TODO: flow
-
-// raincheck !!
-
-// although
-// wheneverjs
-// meanwhilejs
-// meantime
-// conditionjs
-// state-observe
-// state-react
-// on-state
-// state-act
-// act-on
-// acton
-// call-when
-// call-on
-// call-on-me :)
-// act-when
-// redux-do-when
-// act-on-state
-// do-when-changed
-// state-watch
-
-// beetje ver gezocht misschien:
-// seasonal
-
-
-// obtainer
-// redux-act-on
-// conditional-side-effects
-
-
 import doWhen from './doWhen'
 import doWhenTrue from './doWhenTrue'
 import doWhenChanged from './doWhenChanged'
 import createMiddleware from './createMiddleware'
-
-// TODO: Clearer names:
 import doForAll from './doForAll'
-
-
-// doWhen((state, call) => {
-//   Object.keys(state).map((object, key) => call(func, [object], item))
-// })(["key1"])
-
-// doForAll(object => func(object))(["key1": {object: "adfa"}])
-
-
-// doWhen((state, call) => {
-//   state.map(item => call(func, [item], item))
-// })(["key1"])
-
-// doForAllKeys(key => func(key))(["key1"])
-
-
-
 import doForAllKeys from './doForAllKeys'
-
-// doForList
-//
 
 export {
   doWhen,
@@ -71,18 +15,4 @@ export {
   createMiddleware
 }
 
-// doForAllObjects ??
-// doFor ??
-
-// act(() => {}).forAll()
-
-// doWhen(isLoggedIn, connect).then()
-
-// doWhen(isLogginIn, connect).observe(observable => observable.)
-
-/*
-doWhenTrue()
-  .map(s => s.value)
-  .then(connect)
-  .check({value: true})
-*/
+export default doWhen
