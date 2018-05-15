c74a2bbffebb92e83cda1f3b0d4dd2b6a3a93e18
Stijn van der Laan <stijn@demonsters.com>
2018-05-15 10:10:13 +0200

    Revert doWhenTrue to match unit tests & add deprecation warning

--- src/doWhenTrue/index.js
+++ src/doWhenTrue/index.js
@@ -1,9 +1,54 @@
 import createNext from './../_libs/createChainAPI';
-import createConstruct from '../_libs/createConstruct';
+import createConstruct from '../_libs/createConstruct'
+
+
 import doWhen from '../doWhen'
 
-export default function doWhenTrue(constructFunc) {
-  return doWhen((state, call) => {
-    if (state) call((val, next) => constructFunc(next))
-  })
+const doWhenTrue = (defaultValue, options) => {
+
+  console.log("doWhenTrue is deprecated use 'when' instead")
+
+  const create = (constructFunc, defaultValue) => {
+  
+    const c = createConstruct((selector, constructMock, destructMock) => {
+  
+      let oldState = undefined
+      let destruct
+  
+      return (state, ...args) => {
+        let newState = selector(state)
+        if (newState !== oldState) {
+          if (destruct) destruct()
+          if (newState) {
+            const tmp = oldState
+            oldState = newState
+            if (constructMock) {
+              constructMock(constructFunc) // need key?
+            } else {
+              destruct = createNext(next => constructFunc(next, ...args))
+            }
+          }
+        }
+      }
+    })
+    
+    if (defaultValue !== undefined) {
+      c(defaultValue)
+    }
+    return c
+  }
+
+  if (typeof defaultValue === "function") {
+    return create(defaultValue, undefined)
+  }
+
+  if (options) {
+    if (typeof options === "function") {
+      return create(options, defaultValue)
+    }
+    if (options.do) {
+      return create(options.do, defaultValue)
+    }
+  }
 }
+export default doWhenTrue
