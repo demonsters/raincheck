b5661bbb543faf0aba4ccc1f49fb6fb75937c107
Stijn van der Laan <stijn@demonsters.com>
2018-05-15 10:08:46 +0200

    Remove old code from forEachKey

--- src/forEachKey/index.js
+++ src/forEachKey/index.js
@@ -4,29 +4,6 @@ import doWhen from '../doWhen'
 
 const forEachKey = (defaultValue, options) => {
 
-  let cachedValues
-  let cachedObject
-  let keys
-  const keyExtractor = (s, i) => keys ? keys[i] : 0
-
-  if (options) {
-    if (typeof options === "function") {
-      options = {
-        keyExtractor,
-        do: options
-      }
-    } else if (options.do) {
-      options = {
-        ...options,
-        keyExtractor
-      }
-    }
-  } else {
-    options = {
-      keyExtractor
-    }
-  }
-
   const create = (constructFunc, defaultValue, keyExtractor = s => s, changed) => {
 
     let cachedObjects
